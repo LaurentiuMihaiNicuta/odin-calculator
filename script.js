3a40a6a11bd2ba0b0c1f13af20e567d7fcb6fd74
LaurentiuMihaiNicuta <laurentiumihai.nicuta07@gmail.com>
2023-11-06 10:32:37 -1000

    Now it's fully working even with successive operations

--- script.js
+++ script.js
@@ -4,25 +4,32 @@ let secondNumber = 0;
 let counter = 0;
 let operator = ' '; 
 let operatorCheck = 0;
-
-let bigNumberOne = [];
+let bigNumberOne = []; 
 let bigNumberTwo = [];
 
+
 const display = document.querySelector("#displayPart");
+// added firstNumber = add(a,b)/subtract(a,b) etc , so even if we add another number after
+// the firstNumber in getInput function will act like the final result is the firstNumber
+// and add the rest to it so we can go 22 - 5 then 17 + 2 , etc 
 
 function add(a , b){
+    firstNumber = a + b;
     return a + b;
 }
 
 function substract(a , b ){
+    firstNumber = a - b;
     return a - b;
 }
 
 function multiply(a , b ){
+    firstNumber = a * b;
     return a * b;
 }
 function divide(a , b ){
-     return a / b;
+    firstNumber = a / b;
+    return a / b ; 
 }
 
 /*function getInput(number){
@@ -40,17 +47,18 @@ function divide(a , b ){
  old function 
 */
 
+
 function getInput(number){
     
 
     if(operatorCheck == 0){
         firstNumber = 0;
 
-        bigNumberOne.push(number);
-        firstNumber = (createNumber(bigNumberOne,firstNumber));
+        bigNumberOne.push(number); // we add into an array 
+        firstNumber = (createNumber(bigNumberOne,firstNumber)); // we use this function to create a number out of that array
 
         updateDisplay(firstNumber);
-        console.log(firstNumber);
+        console.log(firstNumber); 
 
     }
     if( operatorCheck == 1 ){
@@ -70,6 +78,7 @@ function updateDisplay(number){
 }
 
 function equals(){
+
     if (operator == '+'){
         updateDisplay(add(firstNumber,secondNumber));
     }
@@ -83,20 +92,21 @@ function equals(){
         updateDisplay(divide(firstNumber,secondNumber))
     }
 
+
     resetValues();
 
 }
 
 function changeOperator(newOperator){
     operator = newOperator;
-    updateDisplay(newOperator);
-    operatorCheck = 1;
-    
+    updateDisplay(newOperator); // updating the display into the operator
+    operatorCheck += 1;
+    // too se wich function we should use
 
 }
 
 function resetValues(){
-    firstNumber = 0;
+    
     secondNumber = 0;
     bigNumberOne = [];
     bigNumberTwo = [];
@@ -110,4 +120,7 @@ function createNumber(array,initialNumber){
         initialNumber = initialNumber * 10 + array[i];
     }
     return initialNumber;
+
+    // here we create the number out of that array
 }
+
